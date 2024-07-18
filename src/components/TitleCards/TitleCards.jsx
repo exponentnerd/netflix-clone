9631ee3af759fdf3b433f0366bad061dda1e77c0
exponentnerd <xponenttechnologies@gmail.com>
2024-07-18 09:38:53 +0100

    connected Title card to TMDB movie database

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import { useEffect } from 'react';
 
 
 
 const TitleCards = ({title, category}) => {
 
+
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
 
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWRkODk5MWE3NTBhNTg3MmVmMzQ3MGNjZGU0YmZhYSIsIm5iZiI6MTcyMTI5MDU2OC43NzU1MjgsInN1YiI6IjY2OTdlNzhlYzI1NGEzMjNhMDhhMDYwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.osoZdRFyUsbjaD_4ECUNeEue_NUg0HwQ5NDgE30QP4c'
+    }
+  };
+  
 const handleWheel = (event) =>{
   event.preventDefault();
   cardsRef.current.scrollLeft += event.daltaY;
 }
 
 useEffect(() =>{
+
+   
+  fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=1', options)
+    .then(response => response.json())
+    .then(response => setApiData(response.results))
+    .catch(err => console.error(err));
+
+
   cardsRef.current.addEventListener('wheel', handleWheel);
 }, [])
 
@@ -23,10 +41,10 @@ useEffect(() =>{
     <div className='title-cards'>
       <h2>{title?title: "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-          {cards_data.map((card, index) =>{
+          {apiData.map((card, index) =>{
             return <div className="card" key={index}>
-              <img src={card.image} alt="" />
-              <p>{card.name}</p>
+              <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
+              <p>{card.original_title}</p>
             </div>
 
           }) }
