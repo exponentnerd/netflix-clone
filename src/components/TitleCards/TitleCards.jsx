a4d81b893163b52bf09608ea03e7ed6f6cff4a2b
exponentnerd <xponenttechnologies@gmail.com>
2024-07-18 15:16:10 +0100

    fix the back arrow key and added funtionality to the movie thumbnail

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,14 +1,10 @@
-import React from 'react'
+// import React from 'react'
 import './TitleCards.css'
 import cards_data from '../../assets/cards/Cards_data'
-import { useRef, useState } from 'react'
-import { useEffect } from 'react';
-
-
+import { useRef, useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
 
 const TitleCards = ({title, category}) => {
-
-
   const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
 
@@ -19,38 +15,38 @@ const TitleCards = ({title, category}) => {
       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwYWRkODk5MWE3NTBhNTg3MmVmMzQ3MGNjZGU0YmZhYSIsIm5iZiI6MTcyMTI5MDU2OC43NzU1MjgsInN1YiI6IjY2OTdlNzhlYzI1NGEzMjNhMDhhMDYwZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.osoZdRFyUsbjaD_4ECUNeEue_NUg0HwQ5NDgE30QP4c'
     }
   };
-  
-const handleWheel = (event) =>{
-  event.preventDefault();
-  cardsRef.current.scrollLeft += event.daltaY;
-}
 
-useEffect(() =>{
+  const handleWheel = (event) => {
+    event.preventDefault();
+    cardsRef.current.scrollLeft += event.deltaY;
+  }
 
-   
-  fetch(`https://api.themoviedb.org/3/movie/${category?
-    category:"now_playing"}?language=en-US&page=1`, options)
-    .then(response => response.json())
-    .then(response => setApiData(response.results))
-    .catch(err => console.error(err));
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/${category ? category : "now_playing"}?language=en-US&page=1`, options)
+      .then(response => response.json())
+      .then(response => setApiData(response.results))
+      .catch(err => console.error(err));
 
+    const currentRef = cardsRef.current;
+    currentRef.addEventListener('wheel', handleWheel);
 
-  cardsRef.current.addEventListener('wheel', handleWheel);
-}, [])
+    // Cleanup function to remove the event listener
+    return () => {
+      currentRef.removeEventListener('wheel', handleWheel);
+    };
+  }, [category]);
 
   return (
     <div className='title-cards'>
-      <h2>{title?title: "Popular on Netflix"}</h2>
+      <h2>{title ? title : "Popular on Netflix"}</h2>
       <div className="card-list" ref={cardsRef}>
-          {apiData.map((card, index) =>{
-            return <div className="card" key={index}>
-              <img src={`https://image.tmdb.org/t/p/w500` + card.backdrop_path} alt="" />
-              <p>{card.original_title}</p>
-            </div>
-
-          }) }
+        {apiData.map((card, index) => {
+          return <Link to={`/player/${card.id}`} className="card" key={index}>
+            <img src={`https://image.tmdb.org/t/p/w500${card.backdrop_path}`} alt="" />
+            <p>{card.original_title}</p>
+          </Link>
+        })}
       </div>
-      
     </div>
   )
 }
