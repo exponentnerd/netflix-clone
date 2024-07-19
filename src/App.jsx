0fb6eede435c86312b484497371cf76aa4a5f425
exponentnerd <xponenttechnologies@gmail.com>
2024-07-19 14:11:11 +0100

    installed react toastify to user authentication && fix some bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,38 @@
-// import React from 'react'
-import Home from './pages/Home/Home'
-import { Route, Routes, useNavigate } from 'react-router-dom'
-import Login from './pages/Login/Login'
-import Player from './pages/Player/Player'
-import { onAuthStateChanged } from 'firebase/auth'
-import { useEffect } from 'react'
-import { auth } from './firebase'
+import React, { useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { auth } from './firebase';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Player from './pages/Player/Player';
 
 const App = () => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate
-
-  useEffect(()=>{
-    onAuthStateChanged(auth, async (user) =>{
-      if(user){
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
         console.log("Logged In");
         navigate('/');
-      }else{
-        console.log("Logged Out")
+      } else {
+        console.log("Logged Out");
         navigate('/login');
       }
-    })
-  },[navigate])
+    });
+  }, [navigate]);
 
   return (
     <div>
+      <ToastContainer theme='dark' />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/player/:id' element={<Player/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/player/:id' element={<Player />} />
       </Routes>
- 
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,53 +1,50 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { addDoc, collection, getFirestore } from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC6tP7uH0FG4WlNP4qYjpFHpOQMqyNaHQg",
-    authDomain: "netflix-clone-f4bfe.firebaseapp.com",
-    projectId: "netflix-clone-f4bfe",
-    storageBucket: "netflix-clone-f4bfe.appspot.com",
-    messagingSenderId: "298936343498",
-    appId: "1:298936343498:web:9bbe6f7619e65ba1368d33"
-  };
-  
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-  
-  const signup = async (name, email, password) => {
-      try {
-          const res = await createUserWithEmailAndPassword(auth, email, password);
-          const user = res.user;
-          // Add user information to Firestore
-          await addDoc(collection(db, "users"), {
-              uid: user.uid,
-              name,
-              authProvider: "local",
-              email,
-          });
-      } catch (error) {
-          console.error("Error signing up:", error.message);
-          alert(`Failed to sign up: ${error.message}`);
-      }
-  };
-  
-  const login = async (email, password) => {
-      try {
-          await signInWithEmailAndPassword(auth, email, password);
-      } catch (error) {
-          console.error("Error logging in:", error.message);
-          alert(`Failed to log in: ${error.message}`);
-      }
-  };
-  
-  const logout = async () => {
-      try {
-          await signOut(auth);
-      } catch (error) {
-          console.error("Error logging out:", error.message);
-          alert(`Failed to log out: ${error.message}`);
-      }
-  };
-  
-  export { auth, db, login, signup, logout };
+  apiKey: "AIzaSyC6tP7uH0FG4WlNP4qYjpFHpOQMqyNaHQg",
+  authDomain: "netflix-clone-f4bfe.firebaseapp.com",
+  projectId: "netflix-clone-f4bfe",
+  storageBucket: "netflix-clone-f4bfe.appspot.com",
+  messagingSenderId: "298936343498",
+  appId: "1:298936343498:web:9bbe6f7619e65ba1368d33"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (name, email, password)=>{
+    try{
+       const res = await createUserWithEmailAndPassword(auth,email,password);
+       const user = res.user;
+       await addDoc(collection(db, "user"), {
+        uid: user.uid,
+        name,
+        authProvider:"local",
+        email,
+       });
+    }catch(error){
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async (email, password)=>{
+    try{
+       await signInWithEmailAndPassword(auth, email, password)
+
+    }catch(error){
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+};
+
+const  logout = () =>{
+    signOut(auth)
+}
+
+export {auth, db, login, signup, logout};
+
